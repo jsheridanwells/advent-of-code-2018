8c2dce7c32ab0ef8ee4d6d215921b2d4408d5db4
Jeremy Wells <jsheridanwells@gmail.com>
2018-12-08 08:01:35 -0600

    Day One completed

---
+++ DayOne/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DayOne
+{
+    class Program
+    {
+        static int[] vals = new Input().Values;
+        static void Main(string[] args)
+        {
+            int frequency = 0;
+            
+            foreach (int v in vals)
+            {
+                frequency += v;
+            }
+
+            Console.WriteLine($"The resulting frequency is : { frequency }");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# Advent of Code 2018
+
+My solutions to the [2018 Advent of Code Challenges](https://adventofcode.com/2018).
