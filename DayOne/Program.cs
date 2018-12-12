1f23e6346b8d9d35ba4552c5f866b5228016b0c1
Jeremy Wells <jwells@vertafore.com>
2018-12-12 12:17:19 -0600

    completed day one

--- DayOne/Program.cs
+++ DayOne/Program.cs
@@ -5,17 +5,43 @@ namespace DayOne
 {
     class Program
     {
+        static int frequency = 0;
         static int[] vals = new Input().Values;
+        static List<int> frequencies = new List<int>();
         static void Main(string[] args)
         {
-            int frequency = 0;
-            
-            foreach (int v in vals)
+            int totalFrequency = 0;
+            bool totalFrequencyFound = false;
+            int duplicateFrequency = 0;
+            bool duplicateFrequencyFound = false;
+            int count = 0;
+            while (!duplicateFrequencyFound)
             {
-                frequency += v;
-            }
+                if (count > 0)
+                {
+                    totalFrequencyFound = true;
+                }
 
-            Console.WriteLine($"The resulting frequency is : { frequency }");
+                for (int i = 0; i < vals.Length; i++)
+                {
+                    frequency += vals[i];
+                    if (!totalFrequencyFound)
+                        totalFrequency = frequency;
+                    if (!frequencies.Exists(f => f == frequency))
+                    {
+                        frequencies.Add(frequency);
+                    }
+                    else
+                    {
+                        duplicateFrequencyFound = true;
+                        duplicateFrequency = frequency;
+                        break;
+                    }
+                }
+                count++;
+            }
+            Console.WriteLine($"Total frequency is : {totalFrequency}");
+            Console.WriteLine($"Duplicate frequency is : {duplicateFrequency}");
         }
     }
 }
---
+++ DayOne/TestInput.cs
@@ -0,0 +1,20 @@
+namespace DayOne
+{
+    public class TestInput
+    {
+        public int[] Values
+        {
+            get
+            {
+                return new int[]
+                {
+                    +3,
+                    +3,
+                    +4,
+                    -2,
+                    -4,
+                };
+            }
+        }
+    }
+}
